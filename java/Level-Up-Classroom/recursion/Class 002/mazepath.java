429ccaa0b35d6f73b4ca7d3d1008ce8c13f58f19
Vikas <viktiwari12@gmail.com>
2021-06-22 06:18:11 +0530

    Second class starting

---
+++ java/Level-Up-Classroom/recursion/Class 002/mazepath.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class mazepath 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scn = new Scanner(System.in);
+        int row = scn.nextInt();
+        int col = scn.nextInt();
+    }
+    // 
+    public static ArrayList<String> mazepath_HVD(int sr, int sc, int er, int ec)
+    {
+
+    }
+    public static void mazepath()
+
+}
