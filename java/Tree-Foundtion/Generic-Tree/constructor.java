3dc5c305bdb767e29682e54487e661ca38813e32
Vikas <viktiwari12@gmail.com>
2021-07-12 13:15:06 +0530

    display the tree

--- java/Tree-Foundtion/Generic-Tree/constructor.java
+++ java/Tree-Foundtion/Generic-Tree/constructor.java
@@ -1,15 +1,11 @@
-package Generic
-
+import java.util.*;
 import java.util.ArrayList;
 import java.util.Stack;
 
-import org.w3c.dom.traversal.NodeIterator;
-
--Tree;
 
 public class constructor {
     // make a node class
-    public class node{
+    public static class node{
         int data ;
         // an arraylist to store all the children 
         ArrayList<node> children = new ArrayList<>();
@@ -23,21 +19,46 @@ public class constructor {
             int arr [] = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, -1, 40, 80, -1, 90, -1, -1, -1};
 
             // declear a node that point to the root of the tree
-            node root;
+            node root = null;
             // take a stack 
             Stack<node> st = new Stack<>();
-            for(int i = 0; i< arr.length; i++){
-                // if stack is empty then make the root 
-                if(st.empty()){
-                    root = new node(arr[i]);
-                    st.push(root);
-                } 
-
+            for(int i = 0; i< arr.length ; i++){
                 // if the no is -1 then pop the stack;
-                if(arr[i] == -1) st.pop();
-
-                //  now if the next no is not -1 then make a node and make it the children of the node present on the top of the stack 
-                st.peek().children.add(new node(arr[i]));
+                if(arr[i] == -1){
+                    st.pop();
+                } else{
+                    node t = new node(arr[i]);
+                    // if stack is empty then make the root
+                    if(st.size() == 0){
+                        root = t;
+                    } 
+                    /* now if the next no is not -1 then make a node 
+                    and make it the children of the node present on the top of the stack  */               
+                    else {
+                        st.peek().children.add(t);
+                    }
+                    st.push(t);
+                }
             }
+            // display the tree
+            display(root);
+    }
+    // a function to display the tree
+    public static void display(node root){
+        // first display the root and it's children 
+
+        // take a string and store the root data in it 
+        String st = root.data + ",  ->  ";
+        // add the data of all the children of the root to the string 
+        for(node c : root.children){
+            st += c.data + ", ";
+        }
+        // now print the string 
+        System.out.println(st + ".");
+
+        // now recursively print  the data of all the children of the root
+        for(node rc : root.children){
+            display(rc);
+        }
     }
 }
